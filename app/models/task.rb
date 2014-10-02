3270af45eb0f14e317e7442a9f2d686d827cfdb6
Michael <Michael.ginalick@gmail.com>
2014-10-01 22:14:51 -0500

    twilio pseudo code

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,37 @@
+require 'Date'
 class Task < ActiveRecord::Base
 	belongs_to :user
+
+
+
+	def self.find_due_date
+		date = DateTime.now.to_date
+		tasks = Task.all
+
+		tasks.each do |task|
+			
+			user = task.user_id
+			a    = User.find(user)
+			a.phone
+
+			if date == task.due_date.to_date
+				#call method that instantiates twilio client
+			end
+		end
+	end
+
+	def self.send_text
+   # sid = 'AC015b2287604a70f0e13bb96bc2179f59'
+   # token = '4e93d70cf30464dd672d2aac869938ea'
+   #  # Instantiate a Twilio client
+   #           client = Twilio::REST::Client.new(sid, token)
+   #           from = '+113144417029'
+   #              # Create and send an SMS message
+   #    		 client.account.messages.create(
+   #      	 :from => from,
+   #      	 :to => @user.phone,
+   #      	 :body => "Thanks for signing up."
+   #    )
+	end
+
 end
