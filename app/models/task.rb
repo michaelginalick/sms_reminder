4dcd6af0b859d5b71eba732627698779999500d6
Michael <Michael.ginalick@gmail.com>
2014-10-02 20:42:15 -0500

    twillio method complete

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,37 +1,36 @@
 require 'Date'
 class Task < ActiveRecord::Base
-	belongs_to :user
+belongs_to :user
 
 
 
-	def self.find_due_date
-		date = DateTime.now.to_date
-		tasks = Task.all
+def self.find_due_date_send_text
+	date = DateTime.now.to_date
+	tasks = Task.all
 
-		tasks.each do |task|
-			
-			user = task.user_id
-			a    = User.find(user)
-			a.phone
+	tasks.each do |task|
 
-			if date == task.due_date.to_date
-				#call method that instantiates twilio client
-			end
-		end
-	end
+	user = task.user_id
+	a    = User.find(user)
+	a.phone
 
-	def self.send_text
-   # sid = 'AC015b2287604a70f0e13bb96bc2179f59'
-   # token = '4e93d70cf30464dd672d2aac869938ea'
-   #  # Instantiate a Twilio client
-   #           client = Twilio::REST::Client.new(sid, token)
-   #           from = '+113144417029'
-   #              # Create and send an SMS message
-   #    		 client.account.messages.create(
-   #      	 :from => from,
-   #      	 :to => @user.phone,
-   #      	 :body => "Thanks for signing up."
-   #    )
-	end
+	if date == task.due_date.to_date
+	#call method that instantiates twilio client
+	 sid = 'AC015b2287604a70f0e13bb96bc2179f59'
+     token = '4e93d70cf30464dd672d2aac869938ea'
+#  # Instantiate a Twilio client
+     client = Twilio::REST::Client.new(sid, token)
+     from = '+113144417029'
+     # Create and send an SMS message
+      client.account.messages.create(
+      	 :from => from,
+      	 :to => a.phone,
+      	 :body => "Have you completed #{task.title}?"
+    )
+    end
+  end
+end
+
+
 
 end
